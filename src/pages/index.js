370d7d692b384496a9fe11a9cbe6311d2205551a
Riki <Riki@Rikis-iMac.local>
2018-12-20 14:34:47 +1300

    Added third paragraph.

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <h1>Hello hello hello</h1>
+    <p>This is my test paragraph.</p>
+    <p>Oh look, a second paragraph.</p>
+    <p>Let's go for a third paragraph.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
